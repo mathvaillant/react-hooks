88a8b43e7754bb287e84671a9ece8799d78a1314
mathvaillant <matheusvaillant505@gmail.com>
2021-02-21 20:06:18 +0000

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
-import './App.css'
-import React from 'react'
-import FunctionContextComponent from './FunctionContextComponent'
-import { ThemeProvider } from './ThemeContext'
+import React, { useState, useReducer } from 'react'
+import Todo from './Todo'
+// The magic of useReducer() is:
+// 1  - it takes a dispatch with a type and a payload.
+// The type is essentialy what we are going to do.
+// The payload are the parameters for the action being performed
 
-export default function App() {
+// It's better to store the diff action types in a variable
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo',
+}
+
+// takes the initial state and the action to be fired
+function reducer(todos, action) {
+  // to handle multiple/different states we pass a type (action.type) for each change -> switch(action.type){}
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.payload.id)
+    default:
+      return todos
+  }
+}
+
+// function to handle the action type of add-todo
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false }
+}
+
+function App() {
+  // useReducer takes a function, and a initial state
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // dispatch({type: the action type, payload: the data})
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+    setName('')
+  }
+
   return (
-    // ThemeProvider handles all the login and pushes to all the children down
-    <ThemeProvider>
-      <FunctionContextComponent />
-    </ThemeProvider>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
+    </>
   )
 }
+
+export default App
--- src/ClassContextComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function ClassContextComponent() {
-  return <div></div>
-}
-
-export default ClassContextComponent
