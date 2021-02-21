9ad0b9c385fc0b78b2d99086199451e6386494c8
mathvaillant <matheusvaillant505@gmail.com>
2021-02-21 01:39:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  /* USESTATE HOOK*/
+  // Holds state like in this.state from constructor in classes
+  const [respositories, setRepositories] = useState([])
+
+  /* USEEFFECT HOOK*/
+  // Watches component lifecycle methods
+  useEffect(async () => {
+    const response = await fetch(
+      'https://api.github.com/users/mathvaillant/repos'
+    )
+    const data = await response.json()
+    setRepositories(data)
+  }, [])
+
+  function handleAddRepository() {
+    setRepositories([
+      ...respositories,
+      { id: Math.floor(Math.random()), name: 'new repo' },
+    ])
+  }
+
+  function handleFavorite(id) {
+    const newRepositories = respositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: true } : repo
+    })
+    setRepositories(newRepositories)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        {respositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.favorite && <span>(FAVORITE)</span>} | {repo.name} |{' '}
+            {repo.language}
+            <button onClick={() => handleFavorite(repo.id)}>Favorite</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>Add repositories</button>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/repoStateContext.js
@@ -0,0 +1,4 @@
+const repoState = {
+  done: '✅',
+  todo: '⭕',
+}
