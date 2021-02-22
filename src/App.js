69cda0856f0e115adf4f7338db9f941dc8693f22
mathvaillant <matheusvaillant505@gmail.com>
2021-02-22 20:38:45 +0000

    React typechecking with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import React, { useState, useMemo, useEffect } from 'react'
+import React from 'react'
+import './App.css'
+import PropTypes from 'prop-types'
 
-export default function App() {
-  const [number, setNumber] = useState(0)
-  const [dark, setDark] = useState(false)
-
-  // useMemo() it saves from having to re-render the whole component when there's a value that didn't change
-  // only use it when the function is large. It can cause performance issues
-  const doubleNumber = useMemo(() => {
-    return slowFunction(number)
-    // it takes a dependency to watch for changes
-  }, [number])
-
-  const themeStyles = {
-    backgroundColor: dark ? 'black' : 'white',
-    color: dark ? 'white' : 'black',
-  }
-
-  useEffect(() => {
-    console.log('Theme Changed')
-  }, [themeStyles])
-
+const Test = (props) => {
   return (
-    <>
-      <input
-        type='number'
-        value={number}
-        onChange={(e) => setNumber(parseInt(e.target.value))}
-      />
-      <button onClick={() => setDark((prevDark) => !prevDark)}>
-        Change Theme
-      </button>
-      <div style={themeStyles}>{doubleNumber}</div>
-    </>
+    <div>
+      <h1>{props.str}</h1>
+      <h1>{props.bool ? 'bool' : 'no bool'}</h1>
+      <h1>{props.strOrNum}</h1>
+      <ul>
+        {props.array.map((val) => (
+          <li key={val}>{val}</li>
+        ))}
+      </ul>
+      <br />
+      <ol>
+        {props.objectsArray.map((object) => (
+          <li key={object.age}>{object.name}</li>
+        ))}
+      </ol>
+      <strong>{props.children}</strong>
+    </div>
   )
 }
 
-function slowFunction(num) {
-  console.log('Calling Slow Function')
-  for (let i = 0; i <= 1000000000; i++) {
-    return num * 2
-  }
+Test.propTypes = {
+  str: PropTypes.string.isRequired,
+  bool: PropTypes.bool.isRequired,
+  strOrNum: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number,
+  ]),
+  array: PropTypes.arrayOf(PropTypes.number),
+  objectsArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      age: PropTypes.number.isRequired,
+    })
+  ),
+  children: PropTypes.string.isRequired,
+}
+
+export default function App() {
+  const people = [
+    { name: 'Math Vaillant', age: 23 },
+    { name: 'John Doe', age: 32 },
+  ]
+
+  return (
+    <>
+      <div className='app'></div>
+      <Test
+        str={'mathvaillant'}
+        bool={false}
+        strOrNum={10}
+        array={[1, 2, 3]}
+        objectsArray={people}>
+        Children
+      </Test>
+    </>
+  )
 }
